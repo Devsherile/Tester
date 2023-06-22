a05b146ad9c66a392464ce0672f1316c9506d041
Devsherile <cheptoosherile@gmail.com>
2023-06-22 21:19:45 +0300

    flutter widgets

---
+++ android/app/src/main/kotlin/com/example/myboyf/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.myboyf
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    ));
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("GBWhatsAPP"),
+        centerTitle: true,
+        backgroundColor: Colors.red[600],
+      ),
+      body: Container(
+        color: Colors.grey[400],
+      ),
+      //   child: IconButton(
+      //     onPressed: () {},
+      //     icon: Icon(Icons.alternate_email),
+      //      color: Colors.amber,
+          
+      //   ),
+      // ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text("Click"),
+        backgroundColor: Colors.red[600],
+      ),
+    );
+  }
+}
